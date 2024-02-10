c534e933c6cd6d1a44ef74182d2ebbea062296ff
J10GOF <juandiegoflorezlora@gmail.com>
2024-02-10 11:51:43 -0500

    responcive cards informacion, noticias y inscrpcion

--- src/views/ViewCardHome/ViewCardHome.jsx
+++ src/views/ViewCardHome/ViewCardHome.jsx
@@ -5,10 +5,16 @@ import CardEntrenamiento from "../../Components/CardEntrenamiento/CardEntrnamien
 export default function Viewcardhome() {
   return (
     <div className="flex justify-center p-4 translate-y-2">
-      <div className="flex gap-14">
-        <CardObsequio />
-        <CardEntrenamiento />
-        <Noticias />
+      <div className="flex flex-col sm:flex-row gap-4">
+        <div className="w-full sm:w-1/3">
+          <CardObsequio />
+        </div>
+        <div className="w-full sm:w-1/3">
+          <CardEntrenamiento />
+        </div>
+        <div className="w-full sm:w-1/3">
+          <Noticias />
+        </div>
       </div>
     </div>
   );
