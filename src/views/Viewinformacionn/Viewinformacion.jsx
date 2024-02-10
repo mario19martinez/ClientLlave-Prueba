4cadb42b14135f4153d55ff4b3d9eb37e4d945d7
J10GOF <juandiegoflorezlora@gmail.com>
2024-02-10 11:12:23 -0500

    resolviendo conflicto de aws

--- src/views/Viewinformacionn/Viewinformacion.jsx
+++ src/views/Viewinformacionn/Viewinformacion.jsx
@@ -10,6 +10,6 @@ export default function Viewinformacion () {
       <NavAdmin />
       <SidebarAdmin selectedTab={selectedTab}/>
       <Informacion  classname="-translate-y-96"/>
-    </div>
+    </div> 
   );
 }
