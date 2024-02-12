f58f1dee0e877e256a7a6a6ea06d6f849e001c3f
Mario Martinez <mariomartinez678abc@gmail.com>
2024-02-12 08:10:15 -0500

    Aplicando cambios en las cards de inicio

--- src/Components/CardObsequio/CardObsequio.jsx
+++ src/Components/CardObsequio/CardObsequio.jsx
@@ -1,39 +1,48 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
-import FormObsequio from '../FormObsequio/FormObsequio';
+import { useState } from "react";
+import Modal from "react-modal";
+import FormObsequio from "../FormObsequio/FormObsequio";
 
 const CardObsequio = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    return (
-        <div className="">
-        <div className="w-64 h-60 bg-white shadow-md rounded-md overflow-hidden border-solid border-2 border-blue-500">
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  return (
+    <div className="">
+      <div className="w-64 h-60 bg-gradient-to-br from-blue-400 via-blue-700 to-blue-400 shadow-md rounded-md overflow-hidden border-solid border-2 border-blue-500">
         <div className="p-4">
-        <h2 className="text-lg font-semibold text-gray-800">Regístrate para Obtener</h2>
-        <h2 className="text-lg font-semibold text-gray-800">10 Clases de Obsequio</h2>
-        <p className="text-gray-600 mt-2">¡No te pierdas esta oferta especial!</p>
-      </div>
-      <div className="px-4 py-3 bg-gray-100">
-        <button
-          className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          onClick={() => setModalIsOpen(true)}
-        >
-          Regístrate aquí
-        </button>
-      </div>
+          <h2 className="text-lg font-semibold text-white">
+            Regístrate para Obtener
+          </h2>
+          <h2 className="text-lg font-semibold text-white">
+            10 Clases de Obsequio
+          </h2>
+          <p className="text-white mt-2">
+            ¡No te pierdas esta oferta especial!
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <button
+            className="w-30 py-2 px-4 bg-white font-bold text-blue-600 rounded hover:bg-blue-100 focus:outline-none focus:bg-blue-600"
+            onClick={() => setModalIsOpen(true)}
+          >
+            Regístrate aquí
+          </button>
+        </div>
 
-      {/* Modal para el formulario de registro */}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Registro"
-        className="Modal"
-        overlayClassName="Overlay"
-      >
-        <FormObsequio isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
-      </Modal>
+        {/* Modal para el formulario de registro */}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          contentLabel="Registro"
+          className="Modal"
+          overlayClassName="Overlay"
+        >
+          <FormObsequio
+            isOpen={modalIsOpen}
+            onClose={() => setModalIsOpen(false)}
+          />
+        </Modal>
+      </div>
     </div>
-    </div>
-    );
-  };
-  
-  export default CardObsequio;
+  );
+};
+
+export default CardObsequio;
