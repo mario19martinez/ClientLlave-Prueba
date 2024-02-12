5e00de06980fe816958a5dec89a7fcd949dc791e
Mario Martinez <mariomartinez678abc@gmail.com>
2024-02-12 16:27:35 -0500

    Color de Cards

--- src/Components/CardObsequio/CardObsequio.jsx
+++ src/Components/CardObsequio/CardObsequio.jsx
@@ -6,21 +6,21 @@ const CardObsequio = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="">
-      <div className="w-64 h-60 bg-gradient-to-br from-blue-400 via-blue-700 to-blue-400 shadow-md rounded-md overflow-hidden border-solid border-2 border-blue-500">
+      <div className="w-64 h-60 bg-white shadow-md rounded-md overflow-hidden border-solid border-2 border-blue-500">
         <div className="p-4">
-          <h2 className="text-lg font-semibold text-white">
+          <h2 className="text-lg font-semibold text-gray-700">
             Regístrate para Obtener
           </h2>
-          <h2 className="text-lg font-semibold text-white">
+          <h2 className="text-lg font-semibold text-gray-700">
             10 Clases de Obsequio
           </h2>
-          <p className="text-white mt-2">
+          <p className="text-gray-700 mt-2">
             ¡No te pierdas esta oferta especial!
           </p>
         </div>
         <div className="flex justify-center">
           <button
-            className="w-30 py-2 px-4 bg-white font-bold text-blue-600 rounded hover:bg-blue-100 focus:outline-none focus:bg-blue-600"
+            className="w-30 py-2 px-4 bg-blue-500 font-bold text-white rounded hover:bg-blue-100 focus:outline-none focus:bg-blue-600"
             onClick={() => setModalIsOpen(true)}
           >
             Regístrate aquí
