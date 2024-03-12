31fb7fe6d0729ea7b1592fce2ff71050882860b1
J10GOF <juandiegoflorezlora@gmail.com>
2024-03-11 19:52:39 -0500

    cambios locales

--- src/Components/ModulosNivel/ModuloDetail.jsx
+++ src/Components/ModulosNivel/ModuloDetail.jsx
@@ -171,7 +171,7 @@ function ModuloDetail() {
                   onClick={handleSubmitAnswers}
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 transition duration-300"
                 >
-                  Enviar Respuestas
+                  responder
                 </button>
               </>
             ) : (
