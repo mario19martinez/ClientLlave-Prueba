81af0f685b1f4a8d38f52740c2b6b20adf3882a8
llaveParaLasNaciones <llaveparalasnaciones@gmail.com>
2024-03-30 13:04:55 -0500

    Cambios en cursos campaña

--- src/Components/CursoDetail/CursoDetail.jsx
+++ src/Components/CursoDetail/CursoDetail.jsx
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchCursoDetail } from "../../Redux/features/courses/coursesSlice";
 import { getUserData } from "../../Redux/features/Users/usersSlice";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import EventIcon from "@mui/icons-material/Event";
+//import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+//import EventIcon from "@mui/icons-material/Event";
 
 function CursoDetail() {
   const { id } = useParams();
@@ -45,120 +45,11 @@ function CursoDetail() {
   }
 
   return (
-    <>
-      <div className="hidden sm:block bg-gray-100 p-8 rounded-lg lg:flex">
-        <div className="flex items-start ml-8 lg:w-3/4">
-          {/* Reemplazar la imagen con el video */}
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/dZUnrBc9PXc?autoplay=1&controls=0"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-            className="w-full lg:w-3/5 h-96 object-contain rounded"
-          ></iframe>
-
-          <div className="flex flex-col space-y-4 mt-4 lg:ml-4 lg:mt-0 lg:w-2/5">
-            <h1 className="text-2xl font-bold mb-2 text-gray-900">
-              {cursoDetail.name}
-            </h1>
-            <p className="font-gabarito text-gray-900">
-              <strong>Duración: </strong>
-              {cursoDetail.duracion}
-            </p>
-            <p className="font-gabarito text-gray-900">
-              <strong>Costo: </strong>
-              {cursoDetail.costo}
-            </p>
-            <p className="font-gabarito text-gray-900">
-              <strong>Nivel: </strong>
-              {cursoDetail.nivel}
-            </p>
-            <div className="flex flex-col space-y-4">
-              <p className="text-gray-700 font-semibold text-sm">
-                ¿Quieres más información sobre cómo ser parte del entrenamiento
-                profético? Comunícate con administración:
-              </p>
-              <button
-                onClick={() =>
-                  window.open("https://wa.me/+573126096603", "_blank")
-                }
-                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
-              >
-                <WhatsAppIcon className="mr-2" />
-                Más información
-              </button>
-              <p className="text-gray-700 font-semibold text-sm">
-                ¿Quieres participar en nuestras clases magistrales en vivo con
-                el Apóstol Diego y la Profeta Petra? Únete al grupo de WhatsApp:
-              </p>
-              <button
-                onClick={() =>
-                  window.open(
-                    "https://chat.whatsapp.com/K5QAcc547LF0vlXotYiZat",
-                    "_blank"
-                  )
-                }
-                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
-              >
-                <WhatsAppIcon className="mr-2" />
-                <span className="text-sm">
-                  Grupo de WhatsApp para clases en vivo
-                </span>
-              </button>
-
-              <p className="text-gray-700 font-semibold text-sm">
-                ¿Quieres agendar una cita?
-              </p>
-              <button
-                onClick={() =>
-                  window.open(
-                    "https://calendly.com/llaveparalasnaciones/llamada-personal?month=2024-03",
-                    "_blank"
-                  )
-                }
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
-              >
-                <EventIcon className="mr-2" />
-                Agendar Cita
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-gray-100 p-8 text-gray-800 block sm:hidden">
+      <div className="bg-gray-100 p-8 text-gray-800 block">
         <div className="flex flex-col items-center">
-        <iframe
-              width="100%" 
-              height="380"
-              src="https://www.youtube.com/embed/dZUnrBc9PXc?autoplay=1&controls=0"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-              className="w-full h-64 object-contain rounded-lg mb-4" 
-            ></iframe>
-          <h1 className="text-xl font-bold mb-2">{cursoDetail.name}</h1>
-          <p className="text-base text-gray-700">
-            <strong>Duración: </strong>
-            {cursoDetail.duracion}
-          </p>
-          <p className="text-base text-gray-700">
-              <strong>Costo: </strong>
-              {cursoDetail.costo}
-            </p>
-          <p className="text-base text-gray-700">
-            <strong>Nivel: </strong>
-            {cursoDetail.nivel}
-          </p>
+          <h1 className="font-bold text-3xl mb-2">{cursoDetail.name}</h1>
         </div>
       </div>
-    </>
   );
 }
 
--- src/Components/Cursos/Cursos.jsx
+++ src/Components/Cursos/Cursos.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Vimeo from "@vimeo/player";
 import axios from "axios";
 
 function CursoClases() {
   const { id } = useParams();
   const [clases, setClases] = useState([]);
-  const [claseSeleccionada, setClaseSeleccionada] = useState(null);
   const [descripcion, setDescripcion] = useState("");
 
   useEffect(() => {
@@ -34,25 +32,10 @@ function CursoClases() {
     fetchCursoDescripcion();
   }, [id]);
 
-  useEffect(() => {
-    if (claseSeleccionada && claseSeleccionada.url && claseSeleccionada.platform === "vimeo") {
-      const vimeoPlayer = new Vimeo(`vimeoPlayer-${claseSeleccionada.id}`, {
-        id: claseSeleccionada.url,
-      });
-      return () => {
-        vimeoPlayer.destroy();
-      };
-    }
-  }, [claseSeleccionada]);
-
   const extractYoutubeVideoId = (url) => {
     const regex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const match = url.match(regex);
     return match ? match[1] : null;
-  };
-
-  const handleClaseClick = (clase) => {
-    setClaseSeleccionada(clase);
   };
 
   return (
@@ -61,53 +44,42 @@ function CursoClases() {
       <h2 className="text-2xl font-semibold mb-2 py-6">Clases del Curso</h2>
       <ul className="space-y-4">
         {clases.map((clase) => (
-          <li
-            key={clase.id}
-            className={`cursor-pointer bg-gray-200 p-4 rounded-lg ${
-              claseSeleccionada && claseSeleccionada.id === clase.id ? "bg-blue-200" : ""
-            }`}
-            onClick={() => handleClaseClick(clase)}
-          >
-            <h3 className="text-xl font-semibold mb-2">{clase.name}</h3>
-            {claseSeleccionada && claseSeleccionada.id === clase.id && (
-              <div className="aspect-w-16 aspect-h-9">
-                {claseSeleccionada && claseSeleccionada.url && claseSeleccionada.platform === "vimeo" ? (
-                  <div id={`vimeoPlayer-${claseSeleccionada.id}`} />
-                ) : claseSeleccionada && claseSeleccionada.url && claseSeleccionada.platform === "youtube" ? (
-                  <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
-                    <iframe
-                      title="url"
-                      className="absolute top-0 left-0 w-full h-full"
-                      src={`https://www.youtube.com/embed/${extractYoutubeVideoId(claseSeleccionada.url)}`}
-                      frameBorder="0"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
-                ) : claseSeleccionada && claseSeleccionada.pdfURL ? (
-                  <div>
-                    <ul className="space-y-2">
-                      <li key={claseSeleccionada.id}>
-                        {claseSeleccionada.id === claseSeleccionada?.id && (
-                          <a
-                            href={claseSeleccionada.pdfURL}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center justify-between bg-blue-500 text-white px-4 py-2 rounded-md"
-                          >
-                            <span>{claseSeleccionada.name} PDF</span>
-                            <button className="bg-blue-700 hover:bg-blue-600 text-white px-3 py-1 rounded-md">
-                              Ver Taller
-                            </button>
-                          </a>
-                        )}
-                      </li>
-                    </ul>
-                  </div>
-                ) : (
-                  <p>No hay contenido disponible para esta clase.</p>
-                )}
-              </div>
-            )}
+          <li key={clase.id} className="bg-gray-200 p-4 rounded-lg">
+            
+            <div className="aspect-w-16 aspect-h-9">
+              {clase.url && clase.platform === "youtube" ? (
+                <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+                  <iframe
+                    title={`youtube-${clase.id}`}
+                    className="absolute top-0 left-0 w-full h-full"
+                    src={`https://www.youtube.com/embed/${extractYoutubeVideoId(clase.url)}`}
+                    frameBorder="0"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ) : clase.pdfURL ? (
+                <div>
+                  <ul className="space-y-2">
+                    <li>
+                      <a
+                        href={clase.pdfURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center justify-between bg-blue-500 text-white px-4 py-2 rounded-md"
+                      >
+                        <span>{clase.name} PDF</span>
+                        <button className="bg-blue-700 hover:bg-blue-600 text-white px-3 py-1 rounded-md">
+                          Ver Taller
+                        </button>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <p>No hay contenido disponible para esta clase.</p>
+              )}
+            </div>
+            <h3 className="text-xl font-semibold py-2">{clase.name}</h3>
           </li>
         ))}
       </ul>
