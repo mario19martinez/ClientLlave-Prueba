5f23adb34a3130a88c933a4a6c491abea28d7cfb
J10GOF <juandiegoflorezlora@gmail.com>
2024-04-19 11:10:28 -0500

    Cambio en banners, trasmisiones y contador en home

--- src/Components/Entrenamiento/Banners.jsx
+++ src/Components/Entrenamiento/Banners.jsx
@@ -11,7 +11,6 @@ export default function Banners() {
 
   return (
     <div className="py-5 px-3 flex flex-col md:flex-row">
-      {/* Imagen que ocupa el 50% en pantallas grandes y 100% en móviles */}
       <div className="md:w-1/2">
         <img
           src={banner}
@@ -21,12 +20,11 @@ export default function Banners() {
         />
       </div>
 
-      {/* Contenido de texto que ocupa el 50% en pantallas grandes y 100% en móviles */}
       <div className="md:w-1/2 p-4 flex flex-col justify-center">
-        <h2 className="text-3xl font-bold mb-3 text-gray-800">
+        <h2 className="text-3xl font-bold mb-3 text-gray-800 text-justify">
           ¿Qué es el Entrenamiento Profético?
         </h2>
-        <p className="mb-2 text-gray-700">
+        <p className="mb-2 text-gray-700 text-justify">
           El Entrenamiento Profético comenzó, en cierta forma, como un proceso
           para ayudar a las personas a mejorar sus resultados. Por ello, se
           puede concebir al Entrenamiento Profético como un entrenamiento en
@@ -35,7 +33,7 @@ export default function Banners() {
           quien, desde su centralidad en la vida del hombre, nos revela sus
           diseños para cada vida en particular.
         </p>
-        <p className="text-gray-700">
+        <p className="text-gray-700 text-justify">
           El Entrenamiento Profético, se puede entender como una “técnica de
           entrenamiento, para que se pueda manifestar la voluntad y los diseños
           de Dios para nuestra vida”. En el Entrenamiento Profético el entrenado
@@ -46,12 +44,20 @@ export default function Banners() {
         </p>
       </div>
 
-      {/* Modal para visualizar la imagen completa */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
           <div className="bg-white p-4 rounded-lg">
-            <img src={banner} alt="Banner" style={{ maxWidth: "90vw", maxHeight: "90vh" }}/>
-            <button onClick={toggleModal} className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition duration-300">Cerrar</button>
+            <img
+              src={banner}
+              alt="Banner"
+              style={{ maxWidth: "90vw", maxHeight: "90vh" }}
+            />
+            <button
+              onClick={toggleModal}
+              className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition duration-300"
+            >
+              Cerrar
+            </button>
           </div>
         </div>
       )}
