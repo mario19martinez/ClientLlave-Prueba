27a9e52b380ae510de0ba8afa3199d1f758b10bc
J10GOF <juandiegoflorezlora@gmail.com>
2024-05-16 11:32:39 -0500

    Solucionando error

--- src/App.jsx
+++ src/App.jsx
@@ -133,8 +133,8 @@ import UserDatosDetail from "./Components/Admin/AllUsersAdmin/UsersDatos/UserDat
 
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-//axios.defaults.baseURL = "https://apillave-ebd57605aa78.herokuapp.com/";  
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://apillave-ebd57605aa78.herokuapp.com/";  
 
 const isLoggedIn = localStorage.getItem("isLoggedIn");
 
