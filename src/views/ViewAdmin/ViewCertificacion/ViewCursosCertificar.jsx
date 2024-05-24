f61ce7d3a541ec88a266635449e12550a8a0d119
J10GOF <juandiegoflorezlora@gmail.com>
2024-05-24 15:19:30 -0500

    añadiendo certificado a admin

--- src/views/ViewAdmin/ViewCertificacion/ViewCursosCertificar.jsx
+++ src/views/ViewAdmin/ViewCertificacion/ViewCursosCertificar.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import CursosCertificar from "../../../Components/Admin/Certificacion/CursosCertificar";
 import NavAdmin from "../../../Components/Admin/NavAdmin/NavAdmin";
 import SidebarAdmin from "../../../Components/Admin/SidebarAdmin/SidebarAdmin";
 
 export default function ViewCursosCerticar() {
+  const [selectedTab] = useState("Ceritificados");
   return (
     <div>
       <NavAdmin />
       <div className="flex">
-        <SidebarAdmin />
+        <div className="sticky top-0 h-screen">
+          <SidebarAdmin selectedTab={selectedTab} />
+        </div>
         <CursosCertificar />
       </div>
     </div>
