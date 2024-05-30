6b8b07ac88b8706ee9830b700e722888091941b2
J10GOF <juandiegoflorezlora@gmail.com>
2024-05-30 14:44:34 -0500

    Estilado de clases con barra de progreso

--- src/Components/Cursos/Cursos.jsx
+++ src/Components/Cursos/Cursos.jsx
@@ -1,7 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { FaCheckCircle } from "react-icons/fa";
+import { BsEye } from "react-icons/bs";
 
 function CursoClases() {
   const { id } = useParams();
@@ -53,7 +54,6 @@ function CursoClases() {
     fetchCursoDescripcion();
   }, [id]);
 
-  // Este useEffect es para obtener el progreso de las clases
   useEffect(() => {
     if (userInfo && userInfo.sub) {
       const fetchProgreso = async () => {
@@ -70,8 +70,6 @@ function CursoClases() {
     }
   }, [userInfo, id]);
 
-
-  // useEffect que se encarga del seguimiento de las clases
   useEffect(() => {
     if (claseSeleccionada && claseSeleccionada.url && claseSeleccionada.platform === "youtube") {
       const onYouTubeIframeAPIReady = () => {
@@ -134,7 +132,8 @@ function CursoClases() {
         clearInterval(youtubePlayer.intervalId);
       }
       setYoutubePlayer(null);
-    }  }, [claseSeleccionada, userInfo, id]);
+    }
+  }, [claseSeleccionada, userInfo, id]);
 
   const extractYoutubeVideoId = (url) => {
     const regex =
@@ -144,7 +143,6 @@ function CursoClases() {
   };
 
   const handleClaseClick = async (clase) => {
-    console.log('clase seleccionada:', clase)
     if (claseSeleccionada && claseSeleccionada.id === clase.id) {
       setClaseSeleccionada(null);
     } else {
@@ -153,21 +151,21 @@ function CursoClases() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full px-4 py-2">
+    <div className="flex flex-col items-center justify-center w-full px-6 py-2">
       <div className="mb-6">{descripcion}</div>
       <h2 className="text-2xl font-semibold mb-2 py-6">Clases del Curso</h2>
       <div className="flex mb-4">
         <button
-          className={`mr-4 px-4 py-2 rounded-md ${
-            mostrarTodasClases ? "bg-blue-500 text-white" : "bg-gray-300"
+          className={`mr-4 px-4 py-2 rounded-md transition-colors duration-300 ${
+            mostrarTodasClases ? "bg-blue-500 text-white" : "bg-gray-300 hover:bg-gray-400"
           }`}
           onClick={() => setMostrarTodasClases(true)}
         >
           Todas las Clases
         </button>
         <button
-          className={`px-4 py-2 rounded-md ${
-            !mostrarTodasClases ? "bg-blue-500 text-white" : "bg-gray-300"
+          className={`px-4 py-2 rounded-md transition-colors duration-300 ${
+            !mostrarTodasClases ? "bg-blue-500 text-white" : "bg-gray-300 hover:bg-gray-400"
           }`}
           onClick={() => setMostrarTodasClases(false)}
         >
@@ -179,27 +177,26 @@ function CursoClases() {
           ? clases.map((clase) => (
               <li
                 key={clase.id}
-                className={`cursor-pointer p-4 rounded-lg ${
-                  clase.pdfURL ? "bg-blue-200" : "bg-gray-200"
-                } ${
+                className={`cursor-pointer p-4 rounded-lg transition-transform transform-gpu duration-300 ${
                   claseSeleccionada && claseSeleccionada.id === clase.id
-                    ? "bg-blue-200"
-                    : ""
+                    ? "bg-blue-200 scale-105"
+                    : clase.pdfURL
+                    ? "bg-green-200 hover:scale-105"
+                    : "bg-gray-200 hover:scale-105"
                 }`}
                 onClick={() => handleClaseClick(clase)}
               >
                 <div className="py-1">
                   <h3 className="text-xl font-semibold mb-2">{clase.name}</h3>
-                  {claseSeleccionada && claseSeleccionada.id === clase.id && (
-                    <div className="w-full">
-                      <div className="flex items-center">
+                  {!clase.pdfURL && (
+                    <div className="mb-2">
+                      <div className="flex items-center mb-1">
                         <div className="w-full mr-2">
+                          <div className="text-sm text-gray-600 mb-1">Progreso de la clase</div>
                           <div className="h-2 bg-white rounded-full">
-                          <div
-                              className={`h-2 rounded-full ${
-                                progreso[clase.id] >= 80
-                                  ? "bg-green-500"
-                                  : "bg-yellow-500"
+                            <div
+                              className={`h-2 rounded-full transition-width duration-500 ${
+                                progreso[clase.id] >= 80 ? "bg-green-500" : "bg-yellow-500"
                               }`}
                               style={{
                                 width: `${progreso[clase.id] || 0}%`,
@@ -207,19 +204,21 @@ function CursoClases() {
                             ></div>
                           </div>
                         </div>
-                        <span>{`${(progreso[clase.id] || 0).toFixed(1)}%`}</span>
-                        {/* <span>{`${Math.round(progreso[clase.id] || 0)}%`}</span> */}
+                        <span className="ml-2">{`${(progreso[clase.id] || 0).toFixed(1)}%`}</span>
+                        {progreso[clase.id] >= 80 ? (
+                          <FaCheckCircle className="text-green-500 ml-2" />
+                        ) : (
+                          <BsEye className="text-gray-500 ml-2" />
+                        )}
                       </div>
                     </div>
                   )}
                 </div>
                 <div className="flex items-center justify-between">
-                  <p className="text-sm text-gray-600 mb-4">
-                    Haz clic para ver más
-                  </p>
+                  <p className="text-sm text-gray-600 mb-4">Haz clic para ver más</p>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className={`h-6 w-6 ${
+                    className={`h-6 w-6 transition-transform duration-300 ${
                       claseSeleccionada && claseSeleccionada.id === clase.id
                         ? "transform rotate-180"
                         : ""
@@ -237,17 +236,15 @@ function CursoClases() {
                   </svg>
                 </div>
                 {claseSeleccionada && claseSeleccionada.id === clase.id && (
-                  <div className="aspect-w-16 aspect-h-9">
-                    {claseSeleccionada &&
-                    claseSeleccionada.url &&
-                    claseSeleccionada.platform === "youtube" ? (
-                      <div
-                        className="relative w-full"
-                        style={{ paddingTop: "10.25%" }}
-                      >
-                        <div id={`youtubePlayer-${claseSeleccionada.id}`} />
+                  <div className="aspect-w-16 aspect-h-9 transition-all duration-500">
+                    {claseSeleccionada.url && claseSeleccionada.platform === "youtube" ? (
+                      <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+                        <div
+                          id={`youtubePlayer-${claseSeleccionada.id}`}
+                          className="absolute top-0 left-0 w-full h-full"
+                        />
                       </div>
-                    ) : claseSeleccionada && claseSeleccionada.pdfURL ? (
+                    ) : claseSeleccionada.pdfURL ? (
                       <div>
                         <ul className="space-y-2">
                           <li key={claseSeleccionada.id}>
@@ -276,57 +273,49 @@ function CursoClases() {
             ))
           : clases.length > 0 && (
               <li
-                className={`cursor-pointer p-4 rounded-lg ${
-                  clases[clases.length - 1].pdfURL
-                    ? "bg-blue-200"
-                    : "bg-gray-200"
-                } ${
-                  claseSeleccionada &&
-                  claseSeleccionada.id === clases[clases.length - 1].id
-                    ? "bg-blue-200"
-                    : ""
+                className={`cursor-pointer p-4 rounded-lg transition-transform transform-gpu duration-300 ${
+                  claseSeleccionada && claseSeleccionada.id === clases[clases.length - 1].id
+                    ? "bg-blue-200 scale-105"
+                    : clases[clases.length - 1].pdfURL
+                    ? "bg-green-100 hover:scale-105"
+                    : "bg-gray-200 hover:scale-105"
                 }`}
                 onClick={() => handleClaseClick(clases[clases.length - 1])}
               >
                 <div className="py-1">
-                  <h3>{clases[clases.length - 1].name}</h3>
-                  {claseSeleccionada &&
-                    claseSeleccionada.id === clases[clases.length - 1].id && (
-                      <div className="w-full">
-                        <div className="flex items-center">
-                          <div className="w-full mr-2">
-                            <div className="h-2 bg-gray-200 rounded-full">
+                  <h3 className="text-xl font-semibold mb-2">{clases[clases.length - 1].name}</h3>
+                  {!clases[clases.length - 1].pdfURL && (
+                    <div className="mb-2">
+                      <div className="flex items-center mb-1">
+                        <div className="w-full mr-2">
+                          <div className="text-sm text-gray-600 mb-1">Progreso de la clase</div>
+                          <div className="h-2 bg-white rounded-full">
                             <div
-                                className={`h-full rounded-full ${
-                                  progreso[clases[clases.length - 1].id] >= 80
-                                    ? "bg-green-500"
-                                    : "bg-yellow-500"
-                                }`}
-                                style={{
-                                  width: `${
-                                    progreso[clases[clases.length - 1].id] || 0
-                                  }%`,
-                                }}
-                              ></div>
-                            </div>
+                              className={`h-2 rounded-full transition-width duration-500 ${
+                                progreso[clases[clases.length - 1].id] >= 80 ? "bg-green-500" : "bg-yellow-500"
+                              }`}
+                              style={{
+                                width: `${progreso[clases[clases.length - 1].id] || 0}%`,
+                              }}
+                            ></div>
                           </div>
-                          <span>{`${(progreso[clases[clases.length - 1].id] || 0).toFixed(1)}%`}</span>
-                          {/* <span>{`${Math.round(
-                            progreso[clases[clases.length - 1].id] || 0
-                          )}%`}</span> */}
                         </div>
+                        <span className="ml-2">{`${(progreso[clases[clases.length - 1].id] || 0).toFixed(1)}%`}</span>
+                        {progreso[clases[clases.length - 1].id] >= 80 ? (
+                          <FaCheckCircle className="text-green-500 ml-2" />
+                        ) : (
+                          <BsEye className="text-gray-500 ml-2" />
+                        )}
                       </div>
-                    )}
+                    </div>
+                  )}
                 </div>
                 <div className="flex items-center justify-between">
-                  <p className="text-sm text-gray-600 mb-4">
-                    Haz clic para ver más
-                  </p>
+                  <p className="text-sm text-gray-600 mb-4">Haz clic para ver más</p>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className={`h-6 w-6 ${
-                      claseSeleccionada &&
-                      claseSeleccionada.id === clases[clases.length - 1].id
+                    className={`h-6 w-6 transition-transform duration-300 ${
+                      claseSeleccionada && claseSeleccionada.id === clases[clases.length - 1].id
                         ? "transform rotate-180"
                         : ""
                     }`}
@@ -342,44 +331,37 @@ function CursoClases() {
                     />
                   </svg>
                 </div>
-                {claseSeleccionada &&
-                  claseSeleccionada.id === clases[clases.length - 1].id && (
-                    <div className="aspect-w-16 aspect-h-0">
-                      {claseSeleccionada &&
-                      claseSeleccionada.url &&
-                      claseSeleccionada.platform === "youtube" ? (
-                        <div
-                          className="relative w-full"
-                          style={{ paddingTop: "56.25%" }}
-                        >
-                          <div id={`youtubePlayer-${claseSeleccionada.id}`} />
-                        </div>
-                      ) : claseSeleccionada && claseSeleccionada.pdfURL ? (
-                        <div>
-                          <ul className="space-y-2">
-                            <li key={claseSeleccionada.id}>
-                              {claseSeleccionada.id ===
-                                clases[clases.length - 1].id && (
-                                <a
-                                  href={claseSeleccionada.pdfURL}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  className="flex items-center justify-between bg-blue-500 text-white px-4 py-2 rounded-md"
-                                >
-                                  <span>{claseSeleccionada.name} PDF</span>
-                                  <button className="bg-blue-700 hover:bg-blue-600 text-white px-3 py-1 rounded-md">
-                                    Ver Taller
-                                  </button>
-                                </a>
-                              )}
-                            </li>
-                          </ul>
-                        </div>
-                      ) : (
-                        <p>No hay contenido disponible para esta clase.</p>
-                      )}
-                    </div>
-                  )}
+                {claseSeleccionada && claseSeleccionada.id === clases[clases.length - 1].id && (
+                  <div className="aspect-w-16 aspect-h-9 transition-all duration-500">
+                    {claseSeleccionada.url && claseSeleccionada.platform === "youtube" ? (
+                      <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+                        <div id={`youtubePlayer-${claseSeleccionada.id}`} className="absolute top-0 left-0 w-full h-full" />
+                      </div>
+                    ) : claseSeleccionada.pdfURL ? (
+                      <div>
+                        <ul className="space-y-2">
+                          <li key={claseSeleccionada.id}>
+                            {claseSeleccionada.id === clases[clases.length - 1].id && (
+                              <a
+                                href={claseSeleccionada.pdfURL}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-between bg-blue-500 text-white px-4 py-2 rounded-md"
+                              >
+                                <span>{claseSeleccionada.name} PDF</span>
+                                <button className="bg-blue-700 hover:bg-blue-600 text-white px-3 py-1 rounded-md">
+                                  Ver Taller
+                                </button>
+                              </a>
+                            )}
+                          </li>
+                        </ul>
+                      </div>
+                    ) : (
+                      <p>No hay contenido disponible para esta clase.</p>
+                    )}
+                  </div>
+                )}
               </li>
             )}
       </ul>
