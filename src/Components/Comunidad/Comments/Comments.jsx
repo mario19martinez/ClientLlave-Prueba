28f144d098cbeea521e2963d1a0b9bbd38adf2e5
J10GOF <juandiegoflorezlora@gmail.com>
2024-06-06 15:42:30 -0500

    mejorando estilados y funcionalidad de preguntas de modulos

--- src/Components/Comunidad/Comments/Comments.jsx
+++ src/Components/Comunidad/Comments/Comments.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 
@@ -9,15 +8,14 @@ const Comments = ({ postId }) => {
   const [showComments, setShowComments] = useState(false);
 
   useEffect(() => {
-    // Lógica para recuperar comentarios asociados a postId
     const fetchComments = async () => {
       try {
         const authToken = localStorage.getItem("token");
-      const config = {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      };
+        const config = {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        };
         const response = await axios.get(`/comments/${postId}`, config);
         setComments(response.data.comments);
       } catch (error) {
@@ -45,46 +43,44 @@ const Comments = ({ postId }) => {
 
       if (response.data && response.data.error) {
         console.error("Server error:", response.data.error);
-        // Manejar el error de manera adecuada
       } else {
         setComments([...comments, response.data.comment]);
         setNewComment("");
       }
     } catch (error) {
       console.error("Error adding comment:", error);
-
-      // Imprimir la respuesta del servidor en la consola
       console.log("Server response:", error.response);
     }
   };
 
   const toggleComments = () => {
     setShowComments(!showComments);
-  }
+  };
 
   return (
-    <div className="">
-      <h2 className="text-lx font-bold mb-4">Comments</h2>
-      <div className="flex items-center mb-2">
-        <button onClick={toggleComments} className="mr-2">
+    <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg mt-6">
+      <h2 className="text-xl font-bold mb-4 text-gray-900 dark:text-gray-200">Comentarios</h2>
+      <div className="flex items-center mb-4">
+        <button
+          onClick={toggleComments}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200"
+        >
           {showComments ? "Ocultar" : "Ver Comentarios"}
         </button>
-        <p className="text-gray-500 mb-0">
-          {comments.length === 1
-            ? "1 comment"
-            : `${comments.length} comments`}
+        <p className="text-gray-500 ml-4">
+          {comments.length === 1 ? "1 comentario" : `${comments.length} comentarios`}
         </p>
       </div>
       {showComments && (
-        <ul className="list-none p-0">
+        <ul className="list-none p-0 max-h-64 overflow-y-auto">
           {comments.map((comment) => (
-            <li key={comment.id} className="mb-4 border-b-2 border-gray-300 rounded-lg pb-2 bg-gray-100">
+            <li key={comment.id} className="mb-4 p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-700">
               {comment.user && (
-                <p className="text-gray-800 font-gabarito translate-x-4">
-                  By: {comment.user.name} {comment.user.last_name}
+                <p className="text-gray-800 dark:text-gray-300 font-semibold mb-2">
+                  {comment.user.name} {comment.user.last_name}
                 </p>
               )}
-              <p className="text-gray-800 translate-x-4">{comment.content}</p>
+              <p className="text-gray-800 dark:text-gray-300">{comment.content}</p>
             </li>
           ))}
         </ul>
@@ -93,14 +89,14 @@ const Comments = ({ postId }) => {
         <textarea
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
-          placeholder="Add a comment..."
-          className="w-full p-2 border-2 border-blue-500 rounded"
+          placeholder="Agrega un comentario..."
+          className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-200"
         ></textarea>
         <button
           onClick={handleAddComment}
-          className="bg-blue-500 text-white py-2 px-4 rounded mt-2 hover:bg-blue-700"
+          className="bg-blue-500 text-white py-2 px-4 rounded mt-2 hover:bg-blue-700 transition duration-200"
         >
-          Add Comment
+          Comentar
         </button>
       </div>
     </div>
