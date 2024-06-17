8b9fd898792a9af937574f13c4993cdf50dcfabf
JUAN10GOFL1396 <juan.florez@peaku.io>
2024-06-17 09:07:55 -0500

    resolviendo bug de ordenamiento de modulos

--- src/Components/Admin/NivelAdmin/NivelDetailAdmin.jsx
+++ src/Components/Admin/NivelAdmin/NivelDetailAdmin.jsx
@@ -16,7 +16,6 @@ function NivelDetailAdmin() {
   const [loading, setLoading] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-
   const { id } = useParams();
 
   useEffect(() => {
@@ -26,7 +25,7 @@ function NivelDetailAdmin() {
         setNivel(response.data);
         setLoading(false);
       } catch (error) {
-        console.error("Error al obtener el detail del Nivel:", error);
+        console.error("Error al obtener el detalle del Nivel:", error);
         setError("Se produjo un error al cargar el detalle de este Nivel.");
         setLoading(false);
       }
@@ -64,63 +63,68 @@ function NivelDetailAdmin() {
   };
 
   const goBack = () => {
-    navigate(-1)
-  }
+    navigate(-1);
+  };
 
   return (
-    <div className="absolute top-0 right-36 mt-28 ml-96 p-4 w-3/5 h-auto -translate-x-20">
-      <div className="max-w-xl mx-auto p-4 bg-white shadow rounded-md ">
-      <button
-        onClick={goBack}
-        className="bg-blue-500 text-white w-20 h-10 mb-8 font-semibold py-0 px-4 rounded hover:bg-gray-400 transition-transform ease-in-out duration-300 hover:translate-y-2"
-      >
-        <KeyboardBackspaceIcon fontSize="large" />
-      </button>
-        {loading && <div className="text-center">Cargando...</div>}
-        {error && <div className="text-center text-red-500">{error}</div>}
-        {nivel && (
-          <div>
-            <h2 className="text-2xl font-bold mb-4 text-gray-800">
-              {nivel.name}
-            </h2>
-            <div className="mb-4">
-              <img
-                src={nivel.image}
-                alt="Imagen del nivel"
-                className="w-full h-auto rounded-lg"
-              />
+    <div className="py-5 px-52 items-center justify-center">
+      <div className="max-w-xl mx-auto p-4 bg-white shadow-lg rounded-lg">
+        <button
+          onClick={goBack}
+          className="flex items-center bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-gray-400 transition duration-300 mb-4"
+        >
+          <KeyboardBackspaceIcon fontSize="large" className="mr-2" />
+          Volver
+        </button>
+        {loading ? (
+          <div className="text-center">Cargando...</div>
+        ) : error ? (
+          <div className="text-center text-red-500">{error}</div>
+        ) : (
+          nivel && (
+            <div>
+              <h2 className="text-2xl font-bold mb-4 text-gray-800">{nivel.name}</h2>
+              <div className="mb-4">
+                <img
+                  src={nivel.image}
+                  alt="Imagen del nivel"
+                  className="w-full h-auto rounded-lg"
+                />
+              </div>
+              <p className="text-lg mb-2 text-gray-800">
+                <strong>Duración:</strong> {nivel.duracion}
+              </p>
+              <p className="text-lg mb-2 text-gray-800">
+                <strong>Descripción:</strong> {nivel.description}
+              </p>
+              <p className="text-lg mb-2 text-gray-800">
+                <strong>Costo:</strong> ${nivel.costo}
+              </p>
+              <div className="flex space-x-4 mt-4">
+                <button
+                  onClick={handleDeleteNivel}
+                  className="flex items-center bg-red-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-red-600 transition duration-300"
+                >
+                  <FolderDeleteIcon fontSize="large" className="mr-2" />
+                  Eliminar
+                </button>
+                <button
+                  onClick={handleOpenModal}
+                  className="flex items-center bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300"
+                >
+                  <EditNoteIcon fontSize="large" className="mr-2" />
+                  Editar
+                </button>
+                <button
+                  onClick={handleNavigateToGrupos}
+                  className="flex items-center bg-green-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-green-600 transition duration-300"
+                >
+                  <GroupsIcon fontSize="large" className="mr-2" />
+                  Grupos
+                </button>
+              </div>
             </div>
-            <p className="text-lg mb-2 text-gray-800">
-              <strong>Duración:</strong> {nivel.duracion}
-            </p>
-            <p className="text-lg mb-2 text-gray-800">
-              <strong>Descripción:</strong> {nivel.description}
-            </p>
-            <p className="text-lg mb-2 text-gray-800">
-              <strong>Costo:</strong> ${nivel.costo}
-            </p>
-            <button
-              onClick={handleDeleteNivel}
-              className="bg-red-500 text-white font-semibold py-2 px-2 rounded-md hover:bg-red-600 transition duration-300"
-            >
-              <FolderDeleteIcon fontSize="large" />
-              <h1 className="text-xs text-white">Eliminar</h1>
-            </button>
-            <button
-              onClick={handleOpenModal}
-              className="bg-blue-500 text-white font-semibold py-2 px-3 rounded-md hover:bg-blue-700 transition duration-300 ml-4"
-            >
-              <EditNoteIcon fontSize="large" />
-              <h1 className="text-xs text-white">Editar</h1>
-            </button>
-            <button
-              onClick={() => handleNavigateToGrupos(nivel.id)}
-              className="bg-green-500 text-white font-semibold py-2 px-2 rounded-md hover:bg-green-600 transition duration-300 ml-4"
-            >
-              <GroupsIcon fontSize="large" />
-              <h1 className="text-xs text-white">Grupos</h1>
-            </button>
-          </div>
+          )
         )}
       </div>
       <div>
@@ -134,17 +138,16 @@ function NivelDetailAdmin() {
         shouldCloseOnOverlayClick={true}
         shouldCloseOnEsc={true}
       >
-        <div className="modal-content p-2 w-2/5 h-screen mx-auto rounded-lg shadow-lg">
+        <div className="modal-content p-4 w-full max-w-2xl mx-auto rounded-lg shadow-lg relative">
           <NivelEdit id={id} />
           <button
-            onClick={() => setShowModal(false)}
-            className="absolute top-1 right-1 hover:text-red-400 text-gray-500 rounded-full"
+            onClick={handleCloseModal}
+            className="absolute top-2 right-2 text-gray-500 hover:text-red-400 transition duration-300"
           >
             <CancelIcon fontSize="large" />
           </button>
         </div>
       </Modal>
-     
     </div>
   );
 }
