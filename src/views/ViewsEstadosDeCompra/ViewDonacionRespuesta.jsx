4bc031f6781edd931e3a291f523b01e00a937930
J10GOF <juandiegoflorezlora@gmail.com>
2024-08-01 15:47:07 -0500

    Resolviendo bug de seguimineto y añadiendo respuesta de donacion

---
+++ src/views/ViewsEstadosDeCompra/ViewDonacionRespuesta.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../Components/Footer/Footer";
+import Nav from "../../Components/Nav/Nav";
+import CompraRespuestaDonacion from "../../Components/Planes/Compras/CompraRespuestaDonacion";
+
+export default function ViewDonacionRespuesta() {
+  return (
+    <div>
+      <Nav />
+      <CompraRespuestaDonacion />
+      <Footer />
+    </div>
+  );
+}
