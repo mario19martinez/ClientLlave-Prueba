3a7ce35733d8be33614d54e0da8b099dff29b528
mario19martinez <mariomartinez678abc@gmail.com>
2025-06-19 11:58:34 -0500

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -197,8 +197,8 @@ import ViewModuloUserDetail from "./views/ViewMenuStudent/ViewModulosUser/ViewMo
 
 import axios from "axios";
 
-//axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "https://llaveapi-507c8c7c7bbf.herokuapp.com/";  
+axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "https://llaveapi-507c8c7c7bbf.herokuapp.com/";  
 
 const isLoggedIn = localStorage.getItem("isLoggedIn");
 const userRole = localStorage.getItem("userRole");
